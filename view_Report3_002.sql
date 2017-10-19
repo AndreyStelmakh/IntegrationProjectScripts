b3e26271c510bdfe6e8858d76d947dd014a3218d
Andrei Stelmah <andrey.stelmakh@live.ru>
2017-10-19 15:23:11 +0300

    s

--- view_Report3_002.sql
+++ view_Report3_002.sql
@@ -6,10 +6,16 @@ AS
 
 
 select OrderNumber, Model, Articul,
+
+       StDevPPurchasePricePlan = StDevP( PurchasePricePlan ), -- для проверки верности данных
+       StDevPPurchasePriceFact = StDevP( PurchasePriceFact ), -- для проверки верности данных
+
        AvgPurchasePricePlan = Avg( PurchasePricePlan ),
        AvgPurchasePriceFact = Avg( PurchasePriceFact ),
        SumQuantityPlan = Sum( QuantityPlan ),
        SumQuantityFact = Sum( QuantityFact ),
+       SumPurchaseFact = IsNull(Avg( PurchasePriceFact ) * Sum( QuantityFact ), 0),
+       SumPurchasePlan = IsNull(Avg( PurchasePricePlan ) * Sum( QuantityPlan ), 0),
        Delta = IsNull(Avg( PurchasePriceFact ) * Sum( QuantityFact ), 0) - IsNull(Avg( PurchasePricePlan ) * Sum( QuantityPlan ), 0)
 from( select isnull(f.OrderNumber, r.OrderNumber) as OrderNumber,
              isnull(f.Model, r.Model) as Model,
